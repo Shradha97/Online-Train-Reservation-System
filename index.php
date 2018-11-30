01051043cbab14708d340035bb59799e653ff45a
Shradha97 <35024433+Shradha97@users.noreply.github.com>
2018-11-30 08:20:32 +0530

    added changes

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
-
-<!DOCTYPE html>
-<!This file stores the user requirements like booking tickets etc. in the database>
+<?php
+$con = mysqli_connect("localhost","root","vidya","train");
+if(mysqli_connect_errno())
+{
+	echo "Failed to connect to MYSql: ".mysqli_connect_error();
+}
+?>
 <html>
 <head>
 <title>Railway Booking Portal</title>
@@ -21,33 +25,27 @@ th{text-align: left ;}
 </head>
 <body>
 <?php
-	$fs = interval($_POST['fs']);
-	$ts = interval($_POST['ts']);
-	$d = interval($_POST['d']);
-	$con = mysqli_connect("localhost","root","vaishali","train");
+	$fs = "pune";
+	$ts = "mumbai";
+	$d = "mon";
+	$con = new mysqli("localhost","root","vidya","train");
 echo "<h2>".$fs."</h2>";
-if(mysqli_connect_errno())
-{
-	echo "Failed to connect to MYSql: ".mysqli_connect_error();
+if ($con->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-mysqli_select_db($con,"ajax_demo");
-$sql="select t from station_list as a,station_list as b,train_table as t where a.train_no=b.train_no and a.station = $fs and b.station = $ts and t.{$d}='T' and t.train_no=a.train_no;
-$result =  mysqli_query($con,$sql);
+//mysqli_select_db($con,"ajax_demo");
+$sql="select a.train_no from station_list as a,station_list as b,train_table as t where a.train_no=b.train_no and a.station = '".$fs."' and b.station ='".$ts."' and t.".$d."='T' and t.train_no=a.train_no;";
+echo $sql;
+$result = $con->query($sql);
 
 echo "<table>
 <tr>
 <th>Train Number</th>
-<th>Train Name</th>
-<th>fare</th>
-<th>status</th>
-</tr>;
+</tr>";
 while($row=mysqli_fetch_array($result))
 {
 	echo "<tr>";
 	echo "<td>".$row['train_no']."</td>";
-	echo "<td>".$row['train_name']."</td>";
-	echo "<td>".$row['fare']."</td>";
-	echo "<td>".$row['status']."</td>";
 	echo "</tr>";
 }
 echo "</table>";
